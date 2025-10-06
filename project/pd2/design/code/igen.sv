9658ffeaf376dbb401148e6548639b1a46907299
Anirudh Kaushik <systemani@gmail.com>
2025-10-06 13:50:12 -0400

    update igen interface with insn_i

--- project/pd2/design/code/igen.sv
+++ project/pd2/design/code/igen.sv
@@ -5,12 +5,14 @@
  *
  * Inputs:
  * 1) opcode opcode_i
+ * 2) input instruction insn_i
  * Outputs:
  * 2) 32-bit immediate value imm_o
  */
 
 module igen (
     input logic [6:0] opcode_i,
+    input logic [DWIDTH-1:0] insn_i,
     output logic [31:0] imm_o
 );
     /*
