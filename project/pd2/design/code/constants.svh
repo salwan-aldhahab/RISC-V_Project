1c8f85f5a3edd4018f14134c4d157ca7420440b7
Salwan Aldhahab <91484919+salwan-aldhahab@users.noreply.github.com>
2025-10-08 19:48:45 -0400

    Add RISC-V opcode constants and implement immediate generator
    Defined RISC-V opcode constants in constants.svh and included them in igen.sv. Implemented immediate value extraction logic in the igen module for various instruction types (I, S, B, U, J) based on opcode.

--- project/pd2/design/code/constants.svh
+++ project/pd2/design/code/constants.svh
@@ -13,4 +13,13 @@ parameter logic [31:0] ZERO = 32'd0;
  * Define constants as required...
  */
 
+parameter logic [6:0] OPCODE_RTYPE  = 7'b0110011;
+parameter logic [6:0] OPCODE_ITYPE  = 7'b0010011;
+parameter logic [6:0] OPCODE_LOAD   = 7'b0000011;
+parameter logic [6:0] OPCODE_STORE  = 7'b0100011;
+parameter logic [6:0] OPCODE_BRANCH = 7'b1100011;
+parameter logic [6:0] OPCODE_JALR   = 7'b1100111;
+parameter logic [6:0] OPCODE_JAL    = 7'b1101111;
+parameter logic [6:0] OPCODE_LUI    = 7'b0110111;
+parameter logic [6:0] OPCODE_AUIPC  = 7'b0010111;
 `endif
--- project/pd2/design/code/igen.sv
+++ project/pd2/design/code/igen.sv
@@ -10,7 +10,11 @@
  * 2) 32-bit immediate value imm_o
  */
 
-module igen (
+`include "constants.svh"
+
+module igen #(
+    parameter int DWIDTH=32
+)(
     input logic [6:0] opcode_i,
     input logic [DWIDTH-1:0] insn_i,
     output logic [31:0] imm_o
@@ -19,5 +23,32 @@ module igen (
      * Process definitions to be filled by
      * student below...
      */
+    always_comb begin
+        case (opcode_i)
+            OPCODE_ITYPE, OPCODE_LOAD, OPCODE_JALR: begin
+                // I-type immediate
+                imm_o = {{20{insn_i[31]}}, insn_i[31:20]};
+            end
+            OPCODE_STORE: begin
+                // S-type immediate
+                imm_o = {{20{insn_i[31]}}, insn_i[31:25], insn_i[11:7]};
+            end
+            OPCODE_BRANCH: begin
+                // B-type immediate
+                imm_o = {{19{insn_i[31]}}, insn_i[31], insn_i[7], insn_i[30:25], insn_i[11:8], 1'b0};
+            end
+            OPCODE_LUI, OPCODE_AUIPC: begin
+                // U-type immediate
+                imm_o = {insn_i[31:12], 12'b0};
+            end
+            OPCODE_JAL: begin
+                // J-type immediate
+                imm_o = {{11{insn_i[31]}}, insn_i[31], insn_i[19:12], insn_i[20], insn_i[30:21], 1'b0};
+            end
+            default: begin
+                imm_o = 32'd0; // Default case to avoid latches
+            end
+        endcase
+    end
 
 endmodule : igen
