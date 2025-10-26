165a28472d331dc4fdd14116f8881d8e066e7d1e
Salwan Aldhahab <91484919+salwan-aldhahab@users.noreply.github.com>
2025-10-25 20:56:33 -0400

    Update register_file.sv

--- project/pd3/design/code/register_file.sv
+++ project/pd3/design/code/register_file.sv
@@ -54,12 +54,12 @@
     assign rs2data_o= (regwren_i && (rd_i == rs2_i) && (rd_i != 0)) ? datawb_i : rs2data_raw;
 
     // Writeback logic
-    always_ff @(posedge clk or negedge rst) begin
-        if (!rst) begin
+    always_ff @(posedge clk) begin
+        if (rst) begin
             for (int i = 0; i < 32; i++) begin
                 registers[i] <= '0;
             end
-            registers[2] <= stack_pointer; // Initialize stack pointer (x2) high -> stack grows down
+            //registers[2] <= stack_pointer; // Initialize stack pointer (x2) high -> stack grows down
         end else begin
             if (regwren_i && rd_i != 5'd0) begin
                 registers[rd_i] <= datawb_i;
