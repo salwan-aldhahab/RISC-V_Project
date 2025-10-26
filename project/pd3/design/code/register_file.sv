b5bcce2831c6b7baba83e8617d37ece7cf5ee0af
Salwan Aldhahab <91484919+salwan-aldhahab@users.noreply.github.com>
2025-10-26 16:53:30 -0400

    Update register_file.sv

--- project/pd3/design/code/register_file.sv
+++ project/pd3/design/code/register_file.sv
@@ -41,7 +41,7 @@
      */
 
     // Stack pointer initial value
-    logic [DWIDTH-1:0] stack_pointer = 32'h0100_0000;
+    logic [DWIDTH-1:0] stack_pointer = 32'h0110_0000;
 
     // 32 registers of DWIDTH bits each
     logic [DWIDTH-1:0] registers [31:0];
