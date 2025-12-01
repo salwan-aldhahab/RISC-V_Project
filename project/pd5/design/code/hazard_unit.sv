54adfd4513627d2ef7ada37ec076340e1fabc2f2
Salwan Aldhahab <91484919+salwan-aldhahab@users.noreply.github.com>
2025-11-30 19:38:31 -0500

    Update hazard_unit.sv

--- project/pd5/design/code/hazard_unit.sv
+++ project/pd5/design/code/hazard_unit.sv
@@ -195,7 +195,7 @@ module hazard_unit #(
     always_comb begin
         if (load_use_hazard) begin
             idex_flush = 1'b0;
-        end if (e_opcode == OPCODE_BRANCH) begin
+        end else if (e_opcode == OPCODE_BRANCH) begin
             idex_flush = 1'b0;
         end else if (control_flow_change) begin
             idex_flush = 1'b1;  // Flush for branch/jump
